0fa19c11d892407e551852da88902a0697613a88
juliennoel <124295011+juliennoel22@users.noreply.github.com>
2024-05-29 08:26:28 +0200

    algo

--- src/Arc.java
+++ src/Arc.java
@@ -1,14 +1,15 @@
-public class    Arc {
+public class Arc {
     /*Attributs*/
     String dest; //nom du nœud destination de l’arc
     double cout; //poids de l'arc
 
     /**
-    * Constructeur Arc
-    * @param dest nœud de destination
-    * @param cout poids de l'arc
-    */
-    public Arc(String dest, double cout){
+     * Constructeur Arc
+     *
+     * @param dest nœud de destination
+     * @param cout poids de l'arc
+     */
+    public Arc(String dest, double cout) {
         this.dest = dest;
         this.cout = cout;
     }
